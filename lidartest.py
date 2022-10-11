2d1060367778bdf8831e2fed15fc1763b695f167
Dajor333 <115480864+Dajor333@users.noreply.github.com>
2022-10-11 14:20:06 +0200

    Update lidartest.py

--- lidartest.py
+++ lidartest.py
@@ -1,24 +1,29 @@
 from rplidar import RPLidar
 
 def main():
-    lidar = RPLidar('/dev/ttyUSB0')
+    try:
+        lidar = RPLidar('/dev/ttyUSB0')
 
 
-    info = lidar.get_info()
-    print(info)
+        info = lidar.get_info()
+        print(info)
 
-    health = lidar.get_health()
-    print(health)
+        health = lidar.get_health()
+        print(health)
 
-    for i, scan in enumerate(lidar.iter_scans()):
-        print('%d: Got %d measurments' % (i, len(scan)))
-        createData(scan)
-        if i > 10:
-            break
+        for i, scan in enumerate(lidar.iter_scans()):
+            print('%d: Got %d measurments' % (i, len(scan)))
+            createData(scan)
+            if i > 10:
+                break
 
-    lidar.stop()
-    lidar.stop_motor()
-    lidar.disconnect()
+        lidar.stop()
+        lidar.stop_motor()
+        lidar.disconnect()
+    except RPLidarException:
+        lidar.stop()
+        lidar.disconnect()
+    
 
 def createData(data):
     for daten  in data:
@@ -26,3 +31,4 @@ def createData(data):
 
 
 main()
+
